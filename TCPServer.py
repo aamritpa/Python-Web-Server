0c49332839e5af9f35899527348ccb35222401bc
Abhay Jolly <abhayjolly18@gmail.com>
2021-04-11 15:29:40 -0700

    refactoring

--- TCPServer.py
+++ TCPServer.py
@@ -4,20 +4,6 @@ import codecs
 import time
 from datetime import datetime
 
-monthDict = {
-'01' : 'Jan',
-'02' : 'Feb',
-'03' : 'Mar',
-'04' : 'Apr',
-'05' : 'Ma',
-'06' : 'Jun',
-'07' : 'Jul',
-'08' : 'Aug',
-'09' : 'Sep',
-'10' : 'Oct',
-'11' : 'Nov',
-'12' : 'Dec'
-}
 
 date= '04 Apr 2021 16:59:00'
 serverDate = datetime.strptime(date, '%d %b %Y %H:%M:%S')
